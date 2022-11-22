b20497fba2975244d1fa0ff3874bf015a176b60b
Lucifer <118705361+Lucifer-el@users.noreply.github.com>
2022-11-22 13:52:20 +0700

    Add files via upload

---
+++ ammarsoto.js
@@ -0,0 +1,8 @@
+let text = "";
+let i = 0 ;
+while (i < 10) {
+    text += "<br>nomor nya adalah " + i;
+    i++;
+    if(i%2==1)
+    console.log(i+ " adalah nomor ganjil");
+} 
---
+++ dimassate.js
@@ -0,0 +1,15 @@
+var i = 5
+
+for(i = 0; i < 10; i++)
+if ( i % 2 == 1)
+console.log("nilai saat ini adalah " + i)
+
+
+
+
+
+
+
+
+
+ 
