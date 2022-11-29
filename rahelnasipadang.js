4524aeba7a148e912adcf660eb2fff4174c3f3ed
Lucifer <118705361+Lucifer-el@users.noreply.github.com>
2022-11-29 14:24:22 +0700

    Add files via upload

---
+++ rahelnasipadang.js
@@ -0,0 +1,6 @@
+var input = require('readline-sync')
+var gg = input.question('masukan panjang persegi')
+var ui = input.question('masukan lebar persegi')
+
+var r = gg*ui
+console.log(r + ' ini adalah luas persegi panjangya')
