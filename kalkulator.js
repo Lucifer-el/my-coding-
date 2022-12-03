bcfc971d2abb8d6c7232e9f3908eb71b1d186430
Nasyah pratiwi <118705848+Nsyhprtwi@users.noreply.github.com>
2022-12-03 16:25:51 +0700

    menambahkan inputan dan beberapa fitur
    -menambahkan fitur pembagian dan perkalian.
    -menambahkan inputan untuk penjumlahan, pengurangan, pembagian, dan perkalian.
    -menambahkan output hasil.

--- kalkulator.js
+++ kalkulator.js
@@ -1,7 +1,4 @@
 var cal = require('readline-sync');
-var a = cal.questionInt('input bilangan pertama =');
-var b = cal.questionInt('input bilangan kedua =');
-
 console.log('--------------------')
 console.log('pertambahan = +')
 console.log('pengurangan = -')
@@ -10,10 +7,31 @@ console.log('perkalian = x')
 console.log('--------------------')
 var c = cal.question('pilih metode =')
 
-if (c=='+')
-{console.log(a+b)}
-else {
-  if (c=='-')
-  {console.log(a-b)}
- 
-}
+if (c == '+') { 
+var jumlah = cal.questionInt('input bilangan pertama = ');
+var jumlah2 = cal.questionInt('input bilangan kedua = ');
+var jum = jumlah;
+var jum2 = jumlah2;
+var hasilJum = jum + jum2
+console.log("hasil = " + hasilJum); 
+} if (c == '-') { 
+    var minus = cal.questionInt('input bilangan pertama = ');
+    var minus2 = cal.questionInt('input bilangan kedua = ');
+    var min = minus;
+    var min2 = minus2;
+    var hasilMin2 = min - min2
+    console.log("hasil = " +hasilMin2) 
+}if (c == '/') { 
+    var bagi = cal.questionInt('input bilangan pertama = ');
+    var bagi2 = cal.questionInt('input bilangan kedua = ');
+    var bagi = bagi;
+    var bagi2 = bagi2;
+    var hasil3 = bagi / bagi2;
+      console.log("hasil = " +hasil3) 
+}if (c == '*') { 
+    var kali = cal.questionInt('input bilangan pertama = ');
+    var kali2 = cal.questionInt('input bilangan kedua = ');
+    var kali = kali;
+    var kali2 = kali2;
+    var hasil4 = kali * kali2;
+        console.log("hasil = " +hasil4) }
